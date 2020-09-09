4e0d62dc7b2d162eb63168d443addfc76095ae88
ncthanh9917 <thanh.chi.nguyen>
2020-09-09 09:22:39 +0700

    asdsad

---
+++ .circleci/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build the site
+echo "==================="
+echo "Build the site"
+yarn
+yarn generate
+
+# Prepare test scripts
+echo "==================="
+echo "Prepare test scripts"
+npm i
+npm run compile
+cd ..
+
+echo "==================="
+echo "Finish build"
